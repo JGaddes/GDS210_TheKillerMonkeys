76f7a2745041edb2862f22ab9390db3902a1c92f
Jai Gaddes <1002645@student.sae.edu.au>
2015-10-07 09:33:34 +1100

    Menu

---
+++ Subject44/Assets/Scripts/Menu/CodeScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CodeScript : MonoBehaviour {
+	
+	public List<GameObject> unlockables;
+    public string currentLevel;
+    private int worldIndex;
+    private int levelIndex;
+	private int tester;
+
+	void Start (){
+
+		var input = gameObject.GetComponent<InputField>();
+		var se= new InputField.SubmitEvent();
+		se.AddListener(CodeCheck);
+		input.onEndEdit = se;
+	}
+
+	private void CodeCheck(string other){
+
+        switch (other.ToLower()) {
+		
+		case "bacon":
+			    //Unlocks Levels up to level 2
+			    unlockables[0].gameObject.GetComponent<Image>().enabled = false;
+			tester = 2;
+                UnlockLevel();
+                break;
+		case "sauce":
+                //Unlocks Levels up to level 3
+                unlockables[0].gameObject.GetComponent<Image>().enabled = false;
+                unlockables[1].gameObject.GetComponent<Image>().enabled = false;
+			tester = 3;
+                UnlockLevel();
+                break;
+		default:
+			    Debug.Log ("Wrong Code Nerd!");
+			    break;
+		}
+	}
+
+
+    public void UnlockLevel()
+    {
+
+        for (int i = 0; i < LockLevel.worlds; i++)
+        {
+
+            for (int j = 1; j < tester; j++)
+            {
+
+                worldIndex = (i + 1);
+				levelIndex = (j + 1);
+                PlayerPrefs.SetInt("level" + worldIndex.ToString() + ":" + levelIndex.ToString(), 1);
+            }
+        }
+    }
+}
---
+++ Subject44/Assets/Scripts/Menu/menuScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas exitMenu;
+	public Canvas levelMenu;
+	public Button startText;
+	public Button exitText;
+	public Button levelText;
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+		exitMenu = exitMenu.GetComponent <Canvas> ();
+		startText = startText.GetComponent <Button> ();
+		exitText = exitText.GetComponent <Button> ();
+
+		exitMenu.enabled = false;
+		levelMenu.enabled = false;
+	}
+	
+	public void ExitPress(){
+
+		exitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+		levelText.enabled = false;
+	}
+
+	public void LevelPress(){
+	
+		levelMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+		levelText.enabled = false;
+	}
+
+	public void NoPress(){
+
+		exitMenu.enabled = false;
+		levelMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+		levelText.enabled = true;
+	}
+
+	public void StartLevel(){
+
+		Application.LoadLevel (levelName);
+	}
+
+	public void ExitGame(){
+
+		Application.Quit ();
+		Debug.Log ("You have quit the game");
+	}
+}
