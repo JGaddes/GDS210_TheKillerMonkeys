35d2e1e587fabfdf1006d615d02904e0ad752897
Erin Lorna Hall <erinhall.sk12@hotmail.com>
2015-10-24 15:31:19 +1100

    Map texturing complete

---
+++ Subject44/Assets/Scripts/TestMovment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovment : MonoBehaviour {
+
+	public CharacterController controller;
+	public float speed = 15f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float _vert = -Input.GetAxis("Vertical");
+		float _hori = -Input.GetAxis ("Horizontal");
+		
+		Vector3 _moveDirection = transform.TransformDirection (_hori, 0, _vert).normalized; 
+		controller.SimpleMove(((_moveDirection * speed) * Time.deltaTime) * speed);
+		_moveDirection = Vector3.zero;
+	}
+}
