8777775f796ccfa0082af13b7b2babfb22fa47f0
jarrahholt <1002738@student.sae.edu.au>
2015-11-27 14:11:42 +1100

    Doors tweaked

---
+++ Subject44/Assets/Scripts/BananaMusic2 (1).cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BananaMusic2 : MonoBehaviour {
+	
+	public float maxPitch = 1.5f;
+
+	public AudioReverbPreset reverbPreset;
+	public AudioLowPassFilter lowpassFilter; 
+	public PlayerController player; 
+	AudioSource audio;
+	
+	void Start() 
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	
+	void Update() 
+	{
+		if (player.useBanana) 
+		{
+			if (GetComponent<AudioSource> ().pitch < maxPitch) 
+			{
+				GetComponent<AudioSource> ().pitch += Time.deltaTime; 
+				GetComponent<AudioReverbFilter> ().enabled = true; 
+				GetComponent<AudioLowPassFilter> ().enabled = true; 
+			}
+		} 
+
+		else 
+		{
+			if (GetComponent<AudioSource> ().pitch > 1f)
+			{
+				GetComponent<AudioSource> ().pitch -= Time.deltaTime; 
+				GetComponent<AudioReverbFilter> ().enabled = false; 
+				GetComponent<AudioLowPassFilter> ().enabled = false; 
+			}
+		}
+	}
+}
+
