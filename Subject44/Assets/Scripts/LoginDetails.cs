3b096f0f33e88940227f099d00a861cbf2012cbb
Jai Gaddes <jai.gaddes@gmail.com>
2015-11-30 01:25:04 +1100

    Fixed a few things.

---
+++ Subject44/Assets/Scripts/LoginDetails.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoginDetails : MonoBehaviour {
+
+    public Text loginDetails;
+
+	// Use this for initialization
+	void Start () {
+
+        loginDetails.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+
+        loginDetails.enabled = true;
+    }
+}
